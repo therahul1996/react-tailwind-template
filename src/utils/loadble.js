43726a79a6e917dde65f406837c5f0c31751b7d4
therahulmongoosetech <rahulsahu.mongoosetech@gmail.com>
2024-04-19 17:28:42 +0530

    feat: added lazy loading

---
+++ src/utils/loadble.js
@@ -0,0 +1,13 @@
+import React, { Suspense, lazy } from "react";
+
+const loadble = (importFunc, { fallback = null } = { fallback: null }) => {
+  const LazyCompomponent = lazy(importFunc);
+
+  return (props) => (
+    <Suspense fallback={fallback}>
+      <LazyCompomponent {...props} />
+    </Suspense>
+  );
+};
+
+export default loadble;
